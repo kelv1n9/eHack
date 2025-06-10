903d3a97e44d7c0b13da3f236dc42e9295e4472f
Elvin Gadirov <gadirov190102@gmail.com>
2025-06-10 11:53:56 +0400

    Updated Data Transmission module

--- lib/DataTransmission/DataTransmission.h
+++ lib/DataTransmission/DataTransmission.h
@@ -43,19 +43,106 @@ enum RadioType
     RADIO_NRF24
 };
 
+enum RadioMode
+{
+    Master,
+    Slave
+};
+
 class DataTransmission
 {
 private:
     RadioType currentRadio;
+    RadioMode currentMode;
     RF24 *radioNRF24;
     ELECHOUSE_CC1101 *radioCC1101;
-
+    /**
+     * @brief Initializes the radio module based on the current settings.
+     * @note This function checks the current radio type and mode, and initializes the corresponding radio module.
+     * It is called before sending or receiving packets to ensure the radio is properly configured.
+     * @param None
+     * @return None
+     */
+    void _initRadio();
+    
 public:
+    /**
+    @brief Constructor for DataTransmission class.
+    @param radioPtrNRF Pointer to RF24 radio object.
+    @param radioPtrCC Pointer to ELECHOUSE_CC1101 radio object.
+    @note This constructor initializes the DataTransmission object with pointers to the RF24 and ELECHOUSE_CC1101 radio objects.
+    */
     DataTransmission(RF24 *radioPtrNRF, ELECHOUSE_CC1101 *radioPtrCC);
 
+    /**
+     * @brief Initializes the CC1101 radio module for communication.
+     * @note This function sets up the radio module for either CC1101 or NRF24 based on the current settings.
+     * It configures the radio for either master or slave mode, sets the data rate, payload size, and CRC length.
+     * It also opens the appropriate reading or writing pipes for communication.
+     * @param None
+     * @return None
+     * @note This function is called internally to ensure the radio is properly configured before sending or receiving packets.
+     * 
+     */
     void setRadioCC1101();
-    void setRadioNRF24();
 
-    uint8_t buildCommandPacket(uint8_t type, uint8_t mode, uint8_t action, const uint8_t *payload, uint8_t payloadLen, uint8_t *packetOut);
-    void sendPacket(uint8_t *data, uint8_t len);
+    /**
+     * @brief Initializes the NRF24 radio module for communication.
+     * @note This function sets up the NRF24 radio module for either master or slave mode, configures the data rate,
+     * payload size, and CRC length, and opens the appropriate reading or writing pipes for communication.
+     * @param None
+     * @return None
+     */
+    void setRadioNRF24();
+    /**
+     * @brief Initializes the radio module based on the current settings.
+     * @note This function checks the current radio type and mode, and initializes the corresponding radio module.
+     * It is called before sending or receiving packets to ensure the radio is properly configured.
+     * @param None
+     * @return None
+     */
+    void setSlaveMode();
+    /**
+     * @brief Sets the radio module to master mode.
+     * @note This function configures the radio module to operate in master mode, allowing it to initiate communication.
+     * It is typically used when the device needs to control the communication flow.
+     * @param None
+     * @return None
+     */
+    void setMasterMode();
+
+    /**
+     * @brief Builds a packet for transmission.
+     * @param type The type of the packet (HF/UHF).
+     * @param mode The mode of the packet (e.g., HF/UHF modes).
+     * @param action The action to be performed (enable/disable).
+     * @param payload Pointer to the payload data.
+     * @param payloadLen Length of the payload data.
+     * @param packetOut Pointer to the output buffer for the packet.
+     * @return The length of the built packet.
+     */
+    uint8_t buildPacket(uint8_t type, uint8_t mode, uint8_t action, const uint8_t *payload, uint8_t payloadLen, uint8_t *packetOut);
+    /**
+     * @brief Sends a packet of data.
+     * @param data Pointer to the data to be sent.
+     * @param len Length of the data to be sent.
+     * @return True if the packet was successfully sent, false otherwise.
+     * @note This function initializes the radio and sends the provided data packet.
+     */
+    bool sendPacket(uint8_t *data, uint8_t len);
+    /**
+     * @brief Receives a packet of data.
+     * @param data Pointer to the buffer where received data will be stored.
+     * @param len Pointer to a variable that will hold the length of the received data.
+     * @return True if a packet was successfully received, false otherwise.
+     * @note This function checks for incoming packets and reads them into the provided buffer.
+     */
+    bool receivePacket(uint8_t *data, uint8_t *len);
+    /**
+     * @brief Checks the connection status of the radio module.
+     * @param timeoutMs Timeout in milliseconds for the connection check.
+     * @return True if the connection is established, false otherwise.
+     * @note This function attempts to verify if the radio module is connected and operational within the specified timeout.
+     */
+    bool checkConnection(uint16_t timeoutMs);
 };
